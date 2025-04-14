eb9112ba00e6ca5f90088305d6538b8ac02d1e30
Jessica <j.f.c.p@ua.pt>
2025-04-14 15:45:29 +0100

    teste2

--- gestaoBiblioteca/src/main/java/com/mycompany/gestaobiblioteca/GestaoBiblioteca.java
+++ gestaoBiblioteca/src/main/java/com/mycompany/gestaobiblioteca/GestaoBiblioteca.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.gestaobiblioteca;
 
-/**
- *
- * @author Pedro Almeida
- */
 public class GestaoBiblioteca {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Está feito o trabalho");
+        System.out.println("Hello World!");
     }
 }
