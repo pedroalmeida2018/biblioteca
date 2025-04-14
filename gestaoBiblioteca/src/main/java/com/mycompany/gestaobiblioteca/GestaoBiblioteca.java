9802f5e361b20776e0ed833f6c0ec7f6067a62ed
pedroalmeida2018 <phtalmeida18@ua.pt>
2025-04-14 15:41:45 +0100

    Teste1, Clone;

---
+++ gestaoBiblioteca/src/main/java/com/mycompany/gestaobiblioteca/GestaoBiblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestaobiblioteca;
+
+/**
+ *
+ * @author Pedro Almeida
+ */
+public class GestaoBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Está feito o trabalho");
+    }
+}
